7e0606e20f7841179a68d4d450a126f9b078ad74
DiqouicK-97 <thomas.d97115@gmail.com>
2023-02-06 12:01:45 -0400

    Changement de couleur du badge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesListComponent
+    HeroesListComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroes-list/heroes-list.component.ts
+++ src/app/heroes-list/heroes-list.component.ts
@@ -7,14 +7,22 @@ import Hero from '../types/hero.type';
   styleUrls: ['./heroes-list.component.css']
 })
 export class HeroesListComponent implements OnInit {
-
+  
   heroes: Hero[] = [
-    {id : 1, name : 'Batman' },
-    {id : 2, name : 'Superman' },
-    {id : 3, name : 'Spiderman'}
+    {id : 1, name : 'Batman', superPower : 'Riche'},
+    {id : 2, name : 'Superman', superPower : 'Alien'},
+    {id : 3, name : 'Spiderman', superPower : 'Capacité d\'une arraignée'},
+    {id : 4, name : 'Catwoman', superPower : 'Agilité et Discrétion'},
+    {id : 5, name : 'Wonder Woman', superPower : 'Super-force'},
   ];
 
+  selectedHero : Hero;
+
   constructor() { }
+
+  selectHero(hero : Hero) {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
   }
